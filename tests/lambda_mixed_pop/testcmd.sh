9e9baafb6d042dbbbb7f82c2e85e4ccf5d584873
Jeffrey Barrick <Jeffrey Barrick@localhost>
2010-04-18 19:45:49 -0400

    merged Makefile

---
+++ tests/lambda_mixed_pop/testcmd.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SELF=`dirname ${BASH_SOURCE}`
+. ${SELF}/../../etc/common.sh
+
+testcmd() {
+    ${BRESEQ} --no-junction \
+        -o ${SELF} \
+        -r ${DATADIR}/lambda/lambda.gbk \
+        ${DATADIR}/lambda/lambda_mixed_population.fastq
+}
+
+do_test $1 ${SELF}
