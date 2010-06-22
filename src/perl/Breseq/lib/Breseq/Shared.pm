f0b92d108e2f7cdfe4e49f44814e8564f5837452
Jeffrey Barrick <Jeffrey Barrick@localhost>
2010-06-21 22:01:32 -0400

    first steps of phasing out plot_coverage and coverage.tab file in favor of directly accessing SAM

--- src/perl/Breseq/lib/Breseq/Shared.pm
+++ src/perl/Breseq/lib/Breseq/Shared.pm
@@ -518,6 +518,42 @@ sub junction_name_split
 	return $item;
 }
 
+sub check_region
+{
+	my ($region, $bam) = @_;
+	
+	my ($seq_id, $start, $end);
+	my ($insert_start, $insert_end) = (0, 0);
+	#syntax that includes insert counts
+	# e.g. NC_001416:4566.1-4566.1
+	if ($region =~ m/(.+)\:(\d+)\.(\d+)-(\d+)\.(\d+)/)
+	{	
+		($seq_id, $start, $insert_start, $end, $insert_end) = ($1, $2, $3, $4, $5);
+	}
+	elsif ($region =~ m/(.+)\:(\d+)(\.\.|\-)(\d+)/)
+	{
+		($seq_id, $start, $end) = ($1, $2, $4);
+	}
+	else
+	{
+		($seq_id, $start, $end) = split /:|\.\.|\-/, $region;
+	}
+	my $reference_length = $bam->length($seq_id);
+	
+	($start, $end) = (1, $reference_length) if (!defined $start && !defined $end);
+	$end = $start if (!defined $end);
+	
+	##check the start and end for sanity....	
+	$start = 1 if ($start < 1); 
+	$end = $reference_length if ($end > $reference_length); 
+	
+	die "Problem parsing region: \'$region\'\n" if ($start > $end);
+
+	## return cleaned up region
+	$region = "$seq_id:$start-$end";	
+	return $region;
+}
+
 
 return 1;
 
---
+++ src/perl/Breseq/lib/Breseq/plot_coverage.r
@@ -0,0 +1,48 @@
+## Args should be in_file=/path/to/input out_file=/path/to/output
+
+
+for (e in commandArgs()) {
+  ta = strsplit(e,"=",fixed=TRUE)
+  if(! is.na(ta[[1]][2])) {
+    temp = ta[[1]][2]
+ #   temp = as.numeric(temp) #Im only inputting numbers so I added this to recognize scientific notation
+    if(substr(ta[[1]][1],nchar(ta[[1]][1]),nchar(ta[[1]][1])) == "I") {
+      temp = as.integer(temp)
+    }
+    if(substr(ta[[1]][1],nchar(ta[[1]][1]),nchar(ta[[1]][1])) == "N") {
+      temp = as.numeric(temp)
+    }
+    assign(ta[[1]][1],temp)
+    cat("assigned ",ta[[1]][1]," the value of |",temp,"|\n")
+  } else {
+    assign(ta[[1]][1],TRUE)
+    cat("assigned ",ta[[1]][1]," the value of TRUE\n")
+  }
+}
+
+
+X<-read.table(in_file, sep="\t", header=T)
+X$unique_tot_cov = X$unique_bot_cov + X$unique_top_cov;
+X$redundant_tot_cov = X$redundant_bot_cov + X$redundant_top_cov;
+maxy=max(X$unique_tot_cov, X$redundant_tot_cov) + 5;
+start_pos = X$position[1];
+end_pos = X$position[length(X$position)];
+
+bitmap(out_file, height=450, width=900, type = "png16m", units = "px", res = 72, pointsize=18, taa=4, gaa=2);
+
+par(mar=c(5.1,4.1,1.2,2));
+plot(0:10, 0:10, type="n", lty="solid", ylim=c(0, maxy), xlim=c(start_pos, end_pos), lwd=2, xaxs="i", yaxs="i", xlab="Coordinate in Reference Genome", ylab="Read Coverage Depth")
+
+#### Need to add back the option to gray out the ends!
+#rect(pos[start], 0, pos[del_start], maxy, col="grey85", lty=0)
+#rect(pos[del_end]+1, 0, pos[end], maxy, col="grey85", lty=0)
+
+lines(X$position, X$redundant_tot_cov, type="s", col="red", lty="solid", lwd=2 )
+lines(X$position, X$redundant_top_cov, type="s", col="yellow", lty="solid")
+lines(X$position, X$redundant_bot_cov, type="s", col="orange", lty="solid")
+	
+lines(X$position, X$unique_tot_cov, type="s", col="blue", lty="solid", lwd=2 )
+lines(X$position, X$unique_top_cov, type="s", col="cyan", lty="solid")
+lines(X$position, X$unique_bot_cov, type="s", col="purple", lty="solid")
+
+dev.off()
