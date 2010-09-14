bf256ecbe6aa030fb7a888cb66c77ee20f0a8278
Jeffrey Barrick <Jeffrey Barrick@localhost>
2010-09-14 14:39:39 -0400

    new C++ program to precalculate trims

---
+++ src/c/calc_trims/src/main.cpp
@@ -0,0 +1,197 @@
+#include <boost/program_options.hpp>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+
+#include <faidx.h>
+#include <bam.h>
+
+using namespace std;
+
+/*
+calc_trims
+
+Currently each reference sequence is loaded entirely into memory, 
+which isn't necessary.
+*/
+
+
+// Note: this doesn't quite get the ends of the genome right when repeats overlap there
+bool repeat_match ( const string& _in_seq, const uint32_t pos, const uint32_t repeat_size, const uint32_t repeat_num ) {
+
+  const uint32_t base_offset = repeat_size * repeat_num;
+  for (uint32_t i=0; i<repeat_size; i++) {
+  
+    //cerr << i << " " << _in_seq[pos+i] << " " << _in_seq[pos+i+base_offset] << endl;
+    
+    if (pos+i >= _in_seq.length()) return false;
+    if (_in_seq[pos+i] != _in_seq[pos+i+base_offset]) return false;
+  }
+  return true;
+}
+
+void calculate_trims_1 ( const string& _in_seq, const string& in_output_filename ) {
+
+  //cerr << _in_seq.length() << endl;
+
+  uint8_t * left_trim = new unsigned char[_in_seq.length()];
+  uint8_t * right_trim = new unsigned char[_in_seq.length()];
+  uint32_t max_repeat_length = 18;
+
+  for (unsigned int pos=0; pos<_in_seq.length(); pos++)
+  {
+  
+    // always trim at least one bp
+    uint32_t max_trim_length = 1;
+  
+    //compare starting at this nucleotide
+    for (uint32_t repeat_size=1; repeat_size<=max_repeat_length; repeat_size++)
+    {
+    
+      unsigned int repeat_num = 1;
+      while (repeat_match(_in_seq, pos, repeat_size, repeat_num) ) 
+      {
+        repeat_num++;
+      }
+    
+      //cerr << pos << " " << repeat_size << " " << repeat_num << endl;
+      
+      if (repeat_num > 1)
+      {
+        uint32_t this_trim = repeat_num * repeat_size;
+        if (this_trim > max_trim_length)
+        {
+          max_trim_length = this_trim;
+        }
+      }
+    }
+  
+    // currently limited to an unsigned char
+    if (max_trim_length > 255) max_trim_length = 255;
+    uint8_t add_max_trim_length = max_trim_length;
+  
+    // update relevant trims
+    for (int32_t offset=0; offset<add_max_trim_length; offset++)
+    {
+      if (pos + offset >= _in_seq.length()) break;
+      
+      uint8_t this_trim = offset + 1;
+      right_trim[pos + offset] = max(this_trim, right_trim[pos + offset]);
+    }
+ 
+    for (int32_t offset=add_max_trim_length; offset>=0; offset--)
+    {
+      uint8_t this_trim = add_max_trim_length - offset;
+      left_trim[pos + offset] = max(this_trim, right_trim[pos + offset]);
+    }
+    
+    // debug
+    
+    //if (pos == 10000) break;
+  }
+
+  // Write out the trims to abinary file
+  
+  //cerr << in_output_filename << endl;
+  ofstream out(in_output_filename.c_str(), ios::binary);
+  out << left_trim << right_trim;
+  out.close();
+
+//  debugging...
+//  for (int32_t i=0; i<100; i++)
+//  {
+//    cerr << (i+1) << " " << (int)left_trim[i] << " " << (int)right_trim[i] << endl;
+//  }
+      
+  delete[] left_trim;
+  delete[] right_trim;
+}
+
+void calculate_trims( const string& in_fasta, const string& in_output_path) {
+
+  // Load the sequence index
+  string fai_filename(in_fasta);
+  fai_filename+=".fai";
+  
+  cerr << fai_filename << endl;
+  
+  bam_header_t* bam_header = sam_header_read2(fai_filename.c_str());
+  assert(bam_header);
+  int nseq = bam_header->n_targets;
+
+  faidx_t* fasta_index = fai_load(in_fasta.c_str());
+  assert(fasta_index);
+
+
+	// load all the reference sequences:
+	for(int i=0; i<nseq; ++i) {
+
+		cerr << "  REFERENCE: " << bam_header->target_name[i] << endl;
+		cerr << "  LENGTH: " << bam_header->target_len[i] << endl;
+    
+    int len;
+    const char* cseq = fai_fetch(fasta_index, bam_header->target_name[i], &len);
+    
+    assert(cseq);
+    assert(len > 0);
+    assert(static_cast<unsigned int>(len) == bam_header->target_len[i]);
+    
+    const string seq(cseq);
+    
+    string output_filename(in_output_path);
+    output_filename += "/";
+    output_filename += bam_header->target_name[i];
+    output_filename += ".trims";
+
+    calculate_trims_1(seq, output_filename);
+	}
+  
+  bam_header_destroy(bam_header);
+}
+
+
+
+/*! Identify mutations.
+ 
+ This file only does the command-line parsing bit; the real work is over in
+ identify_mutations.cpp.
+ */
+int main(int argc, char* argv[]) {
+	using namespace std;
+	namespace po = boost::program_options;
+	
+	// setup and parse configuration options:
+	po::options_description cmdline_options("Allowed options");
+	cmdline_options.add_options()
+	("help,h", "produce this help message")
+	("fasta,f", po::value<string>(), "FASTA file of reference sequence")
+	("output,o", po::value<string>(), "output directory")
+  ;
+  
+	po::variables_map options;
+	po::store(po::parse_command_line(argc, argv, cmdline_options), options);
+	po::notify(options);
+	
+	// make sure that the config options are good:
+	if(options.count("help")
+		 || !options.count("fasta")
+		 || !options.count("output")
+		 ) {
+		cout << "Usage: identify_mutations --bam <sequences.bam> --fasta <reference.fasta> --error_dir <path> --genome_diff <path> --output <path> --readfiles <filename> --coverage_dir <dirname> [--minimum-quality-score 3]" << endl;
+		cout << cmdline_options << endl;
+		return -1;
+	}                       
+  
+	// attempt to calculate error calibrations:
+	try {
+		calculate_trims(options["fasta"].as<string>(),options["output"].as<string>());
+		} catch(...) {
+		// failed; 
+		return -1;
+	}
+	
+	return 0;
+}
+
+
--- src/doc/methods.rst
+++ src/doc/methods.rst
@@ -11,6 +11,21 @@ New junction (NJ) evidence
 Read alignment (RA) evidence
 ------------------------------
 
+Read end trimming
+*****************
+
+Alignments of the ends of short reads.
+
+
+1.	Examining portion of read that matches reference
+2.	Examining portion of reference matched by read
+3.	Examining entire read
+4.	Examining extended region of reference
+
+Note to self: we really need to do this once for the entire genome,
+keeping track of how much to trim when end is there from both sides.
+Then we can load this and use it.
+
 
 Missing coverage (MC) evidence
 ------------------------------
