9c4aba250db4430f9a876f66ead6678e9610d0cb
Jeffrey Barrick <Jeffrey Barrick@localhost>
2010-09-30 09:41:26 -0400

    better legends and labels for error rates graph

--- src/perl/Breseq/lib/Breseq/plot_error_rate.r
+++ src/perl/Breseq/lib/Breseq/plot_error_rate.r
@@ -24,6 +24,8 @@ for (e in commandArgs()) {
 X<-read.table(in_file, sep="\t", header=T)
 Y<-X
 
+min_error_rate = 10**floor(log10(min(X)));
+
 pdf(out_file, height=6, width=9)
 
 A_col = "green";
@@ -42,15 +44,15 @@ new_plot <- function(plot_title)
 	#bottom, left, top, right
 	par(mar=c(5,5,3,3));
 	options(warn=-1)
-	plot(0, type="n", lty="solid", log="y", ylim=c(0.00000001, 1), xlim=c(min(X$quality), max(X$quality)), title=plot_title, lwd=1, axes=F, xlab="", ylab="", las=1, cex.lab=1.2, cex.axis=1.2 )
+	plot(0, type="n", lty="solid", log="y", ylim=c(min_error_rate, 1), xlim=c(min(X$quality), max(X$quality)), main=plot_title, lwd=1, axes=F, xlab="", ylab="", las=1, cex.lab=1.2, cex.axis=1.2, cex.main=1.2 )
 	options(warn=1)
 	box()
 	#y-axis
-	axis(2, cex.lab=1.2, las=1, cex.axis=1.2, yaxs="i", at = c(0.00000001, 0.0000001, 0.000001, 0.00001, 0.0001, 0.001, 0.01, 0.1, 1), label = c(expression(10^-8), expression(10^-7), expression(10^-6), expression(10^-5), expression(10^-4), expression(10^-3), expression(10^-2), expression(10^-1), expression(1)))
-	title(ylab="error rate", mgp = c(3.5, 1, 0), cex.lab=1.2)
+	axis(2, cex.lab=1.2, las=1, cex.axis=1.2, yaxs="i", at = c(0.0000000001, 0.000000001, 0.00000001, 0.0000001, 0.000001, 0.00001, 0.0001, 0.001, 0.01, 0.1, 1), label = c(expression(10^-10), expression(10^-9), expression(10^-8), expression(10^-7), expression(10^-6), expression(10^-5), expression(10^-4), expression(10^-3), expression(10^-2), expression(10^-1), expression(1)))
+	title(ylab="Error rate", mgp = c(3.5, 1, 0), cex.lab=1.2)
 	#x-axis
 	axis(1, cex.lab=1.2, cex.axis=1.2, xaxs="i")
-	title(xlab="base quality score", mgp = c(3, 1, 0), cex.lab=1.2)
+	title(xlab="Base quality score", mgp = c(3, 1, 0), cex.lab=1.2)
 }
 
 new_plot("Reference Base: A")
@@ -66,7 +68,7 @@ points(X$quality, X$AG, pch=G_pch, col=G_col)
 lines(Y$quality, Y$A., col=._col)
 points(X$quality, X$A., pch=._pch, col=._col)
 
-legend("topright", c("AC","AT","AG","A."), border="black", horiz=T, fill=c(C_col,T_col,G_col,._col))
+legend("topright", cex=1.1, c("C","T","G","-"), title="Observed Base:", border="black", horiz=T, fill=c(C_col,T_col,G_col,._col), bty="n")
 
 new_plot("Reference Base: T")
 lines(Y$quality, Y$TA, col=A_col)
@@ -81,7 +83,7 @@ points(X$quality, X$TG, pch=G_pch, col=G_col)
 lines(Y$quality, Y$T., col=._col)
 points(X$quality, X$T., pch=._pch, col=._col)
 
-legend("topright", c("TA","TC","TG","T."), border="black", horiz=T, fill=c(A_col,C_col,G_col,._col))
+legend("topright", cex=1.1, c("A","C","G","-"), title="Observed Base:", border="black", horiz=T, fill=c(A_col,C_col,G_col,._col), bty="n")
 
 
 new_plot("Reference Base: C")
@@ -97,7 +99,7 @@ points(X$quality, X$CG, pch=G_pch, col=G_col)
 lines(Y$quality, Y$C., col=._col)
 points(X$quality, X$C., pch=._pch, col=._col)
 
-legend("topright", c("CA","CT","CG","C."), border="black", horiz=T, fill=c(A_col,T_col,G_col,._col))
+legend("topright", cex=1.1, c("A","T","G","-"), title="Observed Base:", border="black", horiz=T, fill=c(A_col,T_col,G_col,._col), bty="n")
 
 
 new_plot("Reference Base: G")
@@ -113,10 +115,10 @@ points(X$quality, X$GC, pch=C_pch, col=C_col)
 lines(Y$quality, Y$G., col=._col)
 points(X$quality, X$G., pch=._pch, col=._col)
 
-legend("topright", c("GA","GT","GC","G."), border="black", horiz=T, fill=c(A_col,T_col,C_col,._col))
+legend("topright", cex=1.1, c("A","T","C","-"), title="Observed Base:", border="black", horiz=T, fill=c(A_col,T_col,C_col,._col), bty="n")
 
 
-new_plot("Reference Base: .")
+new_plot("Reference Base: -")
 lines(Y$quality, Y$.A, col=A_col)
 points(X$quality, X$.A, pch=A_pch, col=A_col)
 
@@ -129,7 +131,7 @@ points(X$quality, X$.C, pch=C_pch, col=C_col)
 lines(Y$quality, Y$.G, col=G_col)
 points(X$quality, X$.G, pch=G_pch, col=G_col)
 
-legend("topright", c(".A",".T",".C",".G"), border="black", horiz=T, fill=c(A_col,T_col,C_col,G_col))
+legend("topright", cex=1.1, c("A","T","C","G"), title="Observed Base:", border="black", horiz=T, fill=c(A_col,T_col,C_col,G_col), bty="n")
 
 
 dev.off()
