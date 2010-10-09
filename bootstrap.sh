f5c46995dc55cbc160eba6010031eec93cc78c61
Jeffrey Barrick <Jeffrey Barrick@localhost>
2010-10-09 10:19:53 -0400

    .

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,15 @@
 #!/bin/sh -e
 
+## make directories
+if [ ! -d aux_build ] ; then
+mkdir aux_build
+fi
+
+if [ ! -d aux_build/m4 ] ; then
+mkdir aux_build/m4
+fi
+
+
 test -n "$srcdir" || srcdir=`dirname "$0"`
 test -n "$srcdir" || srcdir=.
 autoreconf --force --install --verbose "$srcdir"
