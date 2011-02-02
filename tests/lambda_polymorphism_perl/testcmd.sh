c2798b34f66005ff11ed8a2c968f9af5174f4505
Jeffrey Barrick <Jeffrey Barrick@localhost>
2011-02-02 14:27:26 -0600

    misc

---
+++ tests/lambda_polymorphism_perl/testcmd.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SELF=`dirname ${BASH_SOURCE}`
+. ${SELF}/../common.sh
+
+testcmd() {
+    ${BRESEQ} \
+        --perl-identify-mutations \
+    	--polymorphism-prediction \
+        -o ${SELF} \
+        -r ${DATADIR}/lambda/lambda.gbk \
+        ${DATADIR}/lambda/lambda_mixed_population.fastq
+}
+
+do_test $1 ${SELF}
