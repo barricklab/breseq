b5bc4c9ad650388c47bb70434600619995203684
Austin Meyer <Austin Meyer@localhost>
2011-03-29 17:23:27 -0500

    Added guts to the fastq utility

--- src/c/breseq/fastq.cpp
+++ src/c/breseq/fastq.cpp
@@ -20,26 +20,87 @@
 
 namespace breseq {
 
-  cFastqFile::cFastqFile(const std::string &file_name, std::ios_base::openmode mode) {
+  cFastqFile::cFastqFile(const std::string &file_name, std::ios_base::openmode mode)
+  {
+    m_file.open(file_name.c_str(), mode);
+    m_filename          = file_name;
+    m_mode              = mode;
+    m_total_base        = 0;
+    m_max_quality_score = 0;
+    m_min_quality_score = INT_MAX;
+    m_max_read_length   = 0;
+    m_total_reads       = 0;
     
-    // open for reading or writing...
   }
   
   bool cFastqFile::read_sequence(cFastqSequence &sequence) {
     
+    std::string line, longest_read("");
+    uint8_t max_score(0), min_score(UINT8_MAX);
+    uint32_t num_reads(0), num_bases(0);
+    
+    if ( m_file.is_open() ) {
+      while( getline( m_file, line )) {
+        num_reads++;
+        int count(0);
+        
+        while( count < 4 ) {
+          switch (count) {
+            case 0:
+              sequence.m_name = line;
+              getline(m_file, line);
+              break;
+            case 1:
+              sequence.m_sequence = line;
+              getline(m_file, line);
+              break;
+            case 2:
+              getline(m_file, line);
+              break;
+            case 3:
+              sequence.m_qualities = line;
+              break;
+          }
+          count++;
+        }
+        if( sequence.m_sequence.size() > longest_read.size() ) longest_read = line;
+        
+        num_bases += sequence.m_sequence.size();
+        
+        for (uint32_t i=0; i<sequence.m_qualities.size(); i++) {
+          int this_score(uint8_t(sequence.m_qualities[i]));
+          if( this_score > max_score ) max_score = this_score;
+          if( this_score < min_score ) min_score = this_score;
+        }
+        
+      }  
+
+    }
+    m_total_reads = num_reads;
+    m_total_base = num_bases;
+    m_max_read_length = longest_read.size();
+    m_min_quality_score = min_score-33;
+    m_max_quality_score = max_score-33;
+    
+    return true;
   }
 
   void cFastqFile::write_sequence(cFastqSequence &sequence) {
     // don't need to implement in first stage
   }
   
-  void cFastqFile::write_summary_file(std::string filename) {
+  void cFastqFile::write_summary_file() {
     
     // Should have lines like: max_read_length 36
     //                         num_reads 1020200
     //                         min_quality_score 4
     //                         max_quality_score 40
     //                         num_bases 62646176
+    std::cout << "max_read_length "   << m_max_read_length   << std::endl;
+    std::cout << "num_reads "         << m_total_reads       << std::endl;
+    std::cout << "min_quality_score " << m_min_quality_score << std::endl;
+    std::cout << "max_quality_score " << m_max_quality_score << std::endl;
+    std::cout << "num_bases "         << m_total_base        << std::endl;
   }
   
 } // breseq namespace
--- src/c/breseq/fastq_utils_cmdline.cpp
+++ src/c/breseq/fastq_utils_cmdline.cpp
@@ -40,8 +40,8 @@ int analyze_fastq(int argc, char* argv[]) {
 	cmdline_options.add_options()
 	("help,h", "produce this help message")
 	("input,i", po::value<string>(), "input FASTQ File")
+  ("output,o", po::value<string>(),"out to file")
 	("summary,s", po::value<string>(), "output summary file (optional)")
-	("qscore,q", po::value<string>(), "output quality score distribution file (optional)")
   ;
 
   po::variables_map options;
@@ -59,8 +59,12 @@ int analyze_fastq(int argc, char* argv[]) {
   
 	// attempt to calculate error calibrations:
 	try {
-
-//// Put code here... or... send params to another function.    
+    cFastqSequence sequence;
+    
+    cFastqFile fastqparse(options["input"].as<std::string>(), std::fstream::in);
+    
+    fastqparse.read_sequence(sequence);
+    fastqparse.write_summary_file();
     
   } catch(...) {
 		// failed; 
