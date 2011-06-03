b9f4b73b246341d08389f961d55cbb737db9af6f
Jordan Borges <Jordan Borges@localhost>
2011-06-03 17:33:05 -0500

    Continued conversion of shared functions used by CandidateJunction class

--- src/c/breseq/breseq/candidate_junctions.h
+++ src/c/breseq/breseq/candidate_junctions.h
@@ -60,11 +60,11 @@ namespace breseq {
 			int32_t a2_unique_length;
 		};
 
-		static void _alignments_to_candidate_junction(map_t settings, map_t summary, map_t ref_seq_info, map_t fai, map_t header, map_t a1, map_t a2, map_t redundancy_1, map_t redundancy_2);
+		static bool _alignments_to_candidate_junction(Settings settings, Summary summary, const cReferenceSequences& ref_seq_info, faidx_t* fai, bam_header_t* header, bam1_t* a1, bam1_t* a2, int32_t redundancy_1, int32_t redundancy_2);
 		static void _alignments_to_candidate_junctions(Settings settings, Summary summary,  const cReferenceSequences& ref_seq_info, map_t candidate_junctions, faidx_t* fai, bam_header_t* header, vector<bam1_t*> al_ref);
 		static bool _check_read_pair_requirements(Settings settings, int32_t a1_start, int32_t a1_end, int32_t a2_start, int32_t a2_end, int32_t& a1_unique_length, int32_t& a2_unique_length, int32_t& union_length);
 		static void _entire_read_matches(map_t a);
-		static void _num_matches_from_end(map_t a, map_t refseq_str, map_t dir, map_t overlap);
+		static void _num_matches_from_end(bam1_t* a, string refseq_str, bool dir, int32_t overlap, int32_t& qry_mismatch_pos, int32_t& ref_mismatch_pos);
 		static void _split_indel_alignments(Settings settings, Summary summary, bam_header_t* header, ofstream& PSAM, int32_t min_indel_split_len, vector<bam1_t*> al_ref);
 		static void _by_ref_seq_coord(map_t a, map_t b, map_t ref_seq_info);
 		static void _by_score_unique_coord(map_t a, map_t b);
