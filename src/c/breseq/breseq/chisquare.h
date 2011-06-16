3ebff6775544dea9ac43c212a824e28e844cfd91
Jeffrey Barrick <Jeffrey Barrick@localhost>
2011-06-15 22:48:00 -0500

    Fixed chisq to be probability and not cumulative probability. Should be CLEANED UP! Had to rebuild one test because of variance at last sig digit.

--- src/c/breseq/breseq/chisquare.h
+++ src/c/breseq/breseq/chisquare.h
@@ -2,6 +2,9 @@
 
 namespace breseq {
 
+  double gamma(double x);
+  double pchisq(double df, double x);
+  
 	double chisquaredistribution(double v, double x);
 	double incompletegamma(double a, double x, bool complemented = false);
 	double lngamma(double x, double* sgngam);
