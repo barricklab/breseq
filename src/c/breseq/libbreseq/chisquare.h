9a58fb65d43b8d49c60f32e506df630d40174247
Matt Strand <Matt Strand@localhost>
2011-10-20 16:32:11 -0500

    Updated chisquare.cpp and .h to contain the nbdtr function from the cephes cprob library.

--- src/c/breseq/libbreseq/chisquare.h
+++ src/c/breseq/libbreseq/chisquare.h
@@ -8,5 +8,17 @@ namespace breseq {
 	double chisquaredistribution(double v, double x);
 	double incompletegamma(double a, double x, bool complemented = false);
 	double lngamma(double x, double* sgngam);
+  
+  double nbdtrc(int k, int n, double p);
+  double nbdtr(int k, int n, double p);
+  double nbdtri(int k, int n, double p);
+  
+  double incbi(double aa, double bb, double yy0);
+  double incbet(double aa, double bb, double xx);
+  double incbcf(double a, double b, double x);
+  double incbd(double a, double b, double x);
+  double pseries(double a, double b, double x);
+  
+  double ndtri(double y0);
 
 } // namespace breseq
