203532c2ec72615160eaf7bbbc83dc6f868ede15
Aaron Reba <Aaron Reba@localhost>
2012-01-03 14:46:33 -0600

    Added Periodicity

--- src/c/breseq/libbreseq/coverage_distribution.h
+++ src/c/breseq/libbreseq/coverage_distribution.h
@@ -66,11 +66,12 @@ namespace breseq {
     //Tiling is taking a coverage file and grouping entries
     //together in sets of a specified constant in order to produce a condensed
     //file. A constant of 500 will take all entries with positions:
-    //0-499, 500-999, 1000-1499...
+    //1-500, 501-1000, 1001-1500...
     //and average the values in each section to produce a file like so:
-    //0       a
-    //500     b
-    //1000    c
+    //0       0
+    //500     a
+    //1000    b
+    //1500    c
     //Where a, b, and c are the average of all values in the second column
     //in their corresponding section.
     //Written by Aaron Reba
@@ -85,7 +86,7 @@ namespace breseq {
     //segmentation algorithm on the file. This separates the positions of the
     //file into distinct ranges.
     //The produced file might look like so:
-    //0 10
+    //1 10
     //12 30
     //11 11
     //This function also accepts files created by coverage_distribution::tile()
@@ -108,6 +109,29 @@ namespace breseq {
                                 string out_file_name,
                                 string gd_file_name
                                 );
+    
+    //Given a coverage file, this will find the sum of all differences squared
+    //of the specified coverage values element-by-element.
+    
+    //If the method is "ut,ub" the data taken will be the unique_top coverage
+    //and the unique_bot coverage.
+    //If the method is "ut+rt,ut+rb" the data taken will be the unique_top
+    //coverage + the redundant_top coverage and the unique_top coverage +
+    //the redundant_bot coverage.
+    
+    //The offset will shift the second set of data (either the unique_bot or
+    //the unique_top coverage + the redundant_bot coverage), to the left and
+    //append whatever is misaligned to the end of the data.
+    
+    //Written by Aaron Reba
+    static void calculate_periodicity (
+                                      string coverage_file_name,
+                                      string period_file_name,
+                                      uint32_t method,
+                                      uint32_t offset_start,
+                                      uint32_t offset_end,
+                                      uint32_t offset_step
+                                      );
 
 	}; // class CoverageDistribution
 
