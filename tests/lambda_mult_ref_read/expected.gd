06ff509d004c428edbabf1f9b865602fbca6fd5a
Jeffrey Barrick <Jeffrey Barrick@localhost>
2012-01-31 17:09:32 -0600

    Toggled mixed-pop flag back to old setting and updated tests.

--- tests/lambda_mult_ref_read/expected.gd
+++ tests/lambda_mult_ref_read/expected.gd
@@ -10,33 +10,34 @@
 #=READSEQ	tests/lambda_mult_ref_read/../data/lambda/lambda_mixed_population.3.fastq
 #=READSEQ	tests/lambda_mult_ref_read/../data/lambda/lambda_mixed_population.4.fastq
 #=READSEQ	tests/lambda_mult_ref_read/../data/lambda/lambda_mixed_population.5.fastq
-DEL	64	6	NC_001416-0	139	1
-INS	65	7	NC_001416-1	4566	G
-SNP	66	8	NC_001416-2	1261	G
-INS	67	9	NC_001416-2	1435	C
-SNP	68	10	NC_001416-2	2314	A
-DEL	63	58,5	NC_001416-2	2338	5996
-SNP	69	22	NC_001416-3	1915	C
-SNP	70	23	NC_001416-3	5833	G
-DEL	71	24	NC_001416-3	8717	1
-SNP	72	25	NC_001416-4	6817	C
-INS	73	36	NC_001416-4	8156	A
-SNP	74	37	NC_001416-4	8184	T
-SNP	75	38	NC_001416-4	8191	T
-SNP	76	39	NC_001416-4	8203	A
-SNP	77	40	NC_001416-4	8328	G
-SNP	78	41	NC_001416-4	8342	T
-SNP	79	42	NC_001416-4	8442	A
-SNP	80	43	NC_001416-4	8514	A
-SNP	81	44	NC_001416-4	8559	A
-SNP	82	45	NC_001416-4	8597	T
-SNP	83	46	NC_001416-4	8708	C
-SNP	84	47	NC_001416-4	8728	T
-SNP	85	48	NC_001416-4	8774	A
-SNP	86	49	NC_001416-4	8868	C
-SNP	87	50	NC_001416-4	9077	G
-SNP	88	51	NC_001416-4	9172	C
-SUB	89	52,53	NC_001416-4	9176	2	AC
+DEL	52	6	NC_001416-0	139	1
+INS	53	7	NC_001416-1	4566	G
+SNP	54	8	NC_001416-2	1261	G
+INS	55	9	NC_001416-2	1435	C
+SNP	56	10	NC_001416-2	2314	A
+DEL	51	46,5	NC_001416-2	2338	5996
+SNP	57	22	NC_001416-3	1915	C
+SNP	58	23	NC_001416-3	5833	G
+DEL	59	24	NC_001416-3	8717	1
+SNP	60	25	NC_001416-4	6817	C
+INS	61	26	NC_001416-4	8156	A
+SNP	62	27	NC_001416-4	8184	T
+SNP	63	28	NC_001416-4	8191	T
+SNP	64	29	NC_001416-4	8203	A
+SNP	65	30	NC_001416-4	8328	G
+SNP	66	31	NC_001416-4	8342	T
+SNP	67	32	NC_001416-4	8442	A
+SNP	68	33	NC_001416-4	8514	A
+SNP	69	34	NC_001416-4	8559	A
+SNP	70	35	NC_001416-4	8597	T
+SNP	71	36	NC_001416-4	8708	C
+SNP	72	37	NC_001416-4	8728	T
+SNP	73	38	NC_001416-4	8774	A
+SNP	74	39	NC_001416-4	8868	C
+SNP	75	40	NC_001416-4	9077	G
+SNP	76	41	NC_001416-4	9172	C
+SUB	77	42,43	NC_001416-4	9176	2	AC
+SNP	78	44	NC_001416-4	9359	C
 RA	6		NC_001416-0	139	0	G	.	frequency=1	new_cov=34/40	quality=286.5	ref_cov=0/0	tot_cov=34/40
 RA	7		NC_001416-1	4566	1	.	G	frequency=1	new_cov=41/31	quality=199.8	ref_cov=0/0	tot_cov=41/31
 RA	8		NC_001416-2	1261	0	A	G	frequency=1	new_cov=22/58	quality=217.6	ref_cov=0/1	tot_cov=22/60
@@ -57,45 +58,33 @@ RA	22		NC_001416-3	1915	0	T	C	frequency=1	new_cov=57/47	quality=285.9	ref_cov=0/
 RA	23		NC_001416-3	5833	0	A	G	frequency=1	new_cov=21/44	quality=179.7	ref_cov=0/0	tot_cov=21/45
 RA	24		NC_001416-3	8717	0	C	.	frequency=1	new_cov=29/17	quality=171.8	ref_cov=1/0	tot_cov=30/17
 RA	25		NC_001416-4	6817	0	T	C	frequency=1	new_cov=58/78	quality=383.1	ref_cov=0/1	tot_cov=60/79
-RA	26		NC_001416-4	7353	0	G	A	frequency=0.0759	new_cov=8/3	polymorphism_quality=13.6223	quality=351.4	ref_cov=87/47	reject=POLYMORPHISM_FREQUENCY_CUTOFF	tot_cov=96/50
-RA	27		NC_001416-4	7356	0	C	G	frequency=0.0633	new_cov=7/3	polymorphism_quality=14.3389	quality=399.0	ref_cov=102/46	reject=POLYMORPHISM_FREQUENCY_CUTOFF	tot_cov=109/49
-RA	28		NC_001416-4	7361	0	C	A	frequency=0.0500	new_cov=5/3	polymorphism_quality=10.1188	quality=421.6	ref_cov=97/55	reject=POLYMORPHISM_FREQUENCY_CUTOFF	tot_cov=102/58
-RA	29		NC_001416-4	7384	0	G	A	frequency=0.0761	new_cov=6/8	polymorphism_quality=18.9703	quality=449.9	ref_cov=97/73	reject=POLYMORPHISM_FREQUENCY_CUTOFF	tot_cov=103/82
-RA	30		NC_001416-4	7389	0	T	G	frequency=0.1084	new_cov=8/10	polymorphism_quality=24.8417	quality=459.3	ref_cov=81/67	tot_cov=90/77
-RA	31		NC_001416-4	7629	0	T	C	frequency=0.2353	new_cov=16/4	polymorphism_quality=29.2678	quality=146.7	ref_cov=28/37	tot_cov=44/41
-RA	32		NC_001416-4	7796	0	G	A	frequency=0.2000	new_cov=6/9	polymorphism_quality=26.6021	quality=130.6	ref_cov=39/21	tot_cov=45/31
-RA	33		NC_001416-4	7832	0	A	G	frequency=0.1458	new_cov=9/5	polymorphism_quality=19.9779	quality=208.2	ref_cov=43/39	tot_cov=52/44
-RA	34		NC_001416-4	7858	0	C	T	frequency=0.1102	new_cov=11/2	polymorphism_quality=18.7285	quality=249.3	ref_cov=66/39	tot_cov=78/42
-RA	35		NC_001416-4	7879	0	G	C	frequency=0.0902	new_cov=8/3	polymorphism_quality=12.3381	quality=314.3	ref_cov=53/58	reject=POLYMORPHISM_FREQUENCY_CUTOFF	tot_cov=61/61
-RA	36		NC_001416-4	8156	1	.	A	frequency=1	new_cov=25/29	quality=169.6	ref_cov=2/0	tot_cov=27/29
-RA	37		NC_001416-4	8184	0	C	T	frequency=1	new_cov=35/20	quality=167.8	ref_cov=0/0	tot_cov=35/20
-RA	38		NC_001416-4	8191	0	C	T	frequency=1	new_cov=32/17	quality=133.8	ref_cov=2/0	tot_cov=34/17
-RA	39		NC_001416-4	8203	0	G	A	frequency=1	new_cov=12/21	quality=90.4	ref_cov=0/0	tot_cov=12/21
-RA	40		NC_001416-4	8328	0	A	G	frequency=1	new_cov=32/26	quality=166.9	ref_cov=0/0	tot_cov=32/26
-RA	41		NC_001416-4	8342	0	C	T	frequency=1	new_cov=29/23	quality=156.5	ref_cov=1/0	tot_cov=30/24
-RA	42		NC_001416-4	8442	0	G	A	frequency=1	new_cov=25/41	quality=200.6	ref_cov=0/1	tot_cov=25/42
-RA	43		NC_001416-4	8514	0	G	A	frequency=1	new_cov=45/52	quality=296.4	ref_cov=1/0	tot_cov=48/52
-RA	44		NC_001416-4	8559	0	G	A	frequency=1	new_cov=24/45	quality=209.7	ref_cov=0/0	tot_cov=24/45
-RA	45		NC_001416-4	8597	0	C	T	frequency=1	new_cov=33/44	quality=232.7	ref_cov=1/0	tot_cov=34/44
-RA	46		NC_001416-4	8708	0	T	C	frequency=1	new_cov=34/25	quality=166.1	ref_cov=0/0	tot_cov=34/25
-RA	47		NC_001416-4	8728	0	C	T	frequency=1	new_cov=41/41	quality=252.5	ref_cov=0/0	tot_cov=41/42
-RA	48		NC_001416-4	8774	0	C	A	frequency=1	new_cov=32/45	quality=235.0	ref_cov=1/0	tot_cov=33/45
-RA	49		NC_001416-4	8868	0	T	C	frequency=1	new_cov=45/44	quality=250.2	ref_cov=0/0	tot_cov=45/44
-RA	50		NC_001416-4	9077	0	A	G	frequency=1	new_cov=35/51	quality=234.4	ref_cov=0/0	tot_cov=35/51
-RA	51		NC_001416-4	9172	0	T	C	frequency=1	new_cov=44/48	quality=260.6	ref_cov=0/0	tot_cov=44/48
-RA	52		NC_001416-4	9176	0	G	A	frequency=1	new_cov=36/39	quality=218.4	ref_cov=0/0	tot_cov=36/39
-RA	53		NC_001416-4	9177	0	T	C	frequency=1	new_cov=36/38	quality=208.5	ref_cov=0/0	tot_cov=36/39
-RA	54		NC_001416-4	9359	0	T	C	frequency=0.8161	new_cov=35/36	polymorphism_quality=27.5876	quality=148.4	ref_cov=6/10	tot_cov=41/46
-RA	55		NC_001416-4	9401	0	G	A	frequency=0.1310	new_cov=2/9	polymorphism_quality=16.2874	quality=175.3	ref_cov=43/30	tot_cov=45/39
-RA	56		NC_001416-4	9494	0	C	A	frequency=0.1667	new_cov=10/10	polymorphism_quality=34.9380	quality=269.3	ref_cov=53/47	tot_cov=63/57
-MC	57		NC_001416-0	1	2	0	0	left_inside_cov=0	left_outside_cov=NA	right_inside_cov=0	right_outside_cov=157
-MC	58		NC_001416-2	2338	8333	0	0	left_inside_cov=0	left_outside_cov=102	right_inside_cov=0	right_outside_cov=102
-MC	59		NC_001416-4	9651	9701	0	0	left_inside_cov=42	left_outside_cov=43	right_inside_cov=0	right_outside_cov=NA
+RA	26		NC_001416-4	8156	1	.	A	frequency=1	new_cov=25/29	quality=169.6	ref_cov=2/0	tot_cov=27/29
+RA	27		NC_001416-4	8184	0	C	T	frequency=1	new_cov=35/20	quality=167.8	ref_cov=0/0	tot_cov=35/20
+RA	28		NC_001416-4	8191	0	C	T	frequency=1	new_cov=32/17	quality=133.8	ref_cov=2/0	tot_cov=34/17
+RA	29		NC_001416-4	8203	0	G	A	frequency=1	new_cov=12/21	quality=90.4	ref_cov=0/0	tot_cov=12/21
+RA	30		NC_001416-4	8328	0	A	G	frequency=1	new_cov=32/26	quality=166.9	ref_cov=0/0	tot_cov=32/26
+RA	31		NC_001416-4	8342	0	C	T	frequency=1	new_cov=29/23	quality=156.5	ref_cov=1/0	tot_cov=30/24
+RA	32		NC_001416-4	8442	0	G	A	frequency=1	new_cov=25/41	quality=200.6	ref_cov=0/1	tot_cov=25/42
+RA	33		NC_001416-4	8514	0	G	A	frequency=1	new_cov=45/52	quality=296.4	ref_cov=1/0	tot_cov=48/52
+RA	34		NC_001416-4	8559	0	G	A	frequency=1	new_cov=24/45	quality=209.7	ref_cov=0/0	tot_cov=24/45
+RA	35		NC_001416-4	8597	0	C	T	frequency=1	new_cov=33/44	quality=232.7	ref_cov=1/0	tot_cov=34/44
+RA	36		NC_001416-4	8708	0	T	C	frequency=1	new_cov=34/25	quality=166.1	ref_cov=0/0	tot_cov=34/25
+RA	37		NC_001416-4	8728	0	C	T	frequency=1	new_cov=41/41	quality=252.5	ref_cov=0/0	tot_cov=41/42
+RA	38		NC_001416-4	8774	0	C	A	frequency=1	new_cov=32/45	quality=235.0	ref_cov=1/0	tot_cov=33/45
+RA	39		NC_001416-4	8868	0	T	C	frequency=1	new_cov=45/44	quality=250.2	ref_cov=0/0	tot_cov=45/44
+RA	40		NC_001416-4	9077	0	A	G	frequency=1	new_cov=35/51	quality=234.4	ref_cov=0/0	tot_cov=35/51
+RA	41		NC_001416-4	9172	0	T	C	frequency=1	new_cov=44/48	quality=260.6	ref_cov=0/0	tot_cov=44/48
+RA	42		NC_001416-4	9176	0	G	A	frequency=1	new_cov=36/39	quality=218.4	ref_cov=0/0	tot_cov=36/39
+RA	43		NC_001416-4	9177	0	T	C	frequency=1	new_cov=36/38	quality=208.5	ref_cov=0/0	tot_cov=36/39
+RA	44		NC_001416-4	9359	0	T	C	frequency=1	new_cov=35/36	quality=148.4	ref_cov=6/10	tot_cov=41/46
+MC	45		NC_001416-0	1	2	0	0	left_inside_cov=0	left_outside_cov=NA	right_inside_cov=0	right_outside_cov=157
+MC	46		NC_001416-2	2338	8333	0	0	left_inside_cov=0	left_outside_cov=102	right_inside_cov=0	right_outside_cov=102
+MC	47		NC_001416-4	9651	9701	0	0	left_inside_cov=42	left_outside_cov=43	right_inside_cov=0	right_outside_cov=NA
 JC	2		NC_001416-0	9700	-1	NC_001416-1	1	1	0	alignment_overlap=0	coverage_minus=84	coverage_plus=62	flanking_left=35	flanking_right=35	key=NC_001416-0__9700__-1__NC_001416-1__1__1__0____35__35__0__0	max_left=34	max_left_minus=34	max_left_plus=34	max_min_left=17	max_min_left_minus=17	max_min_left_plus=16	max_min_right=17	max_min_right_minus=17	max_min_right_plus=16	max_pos_hash_score=68	max_right=34	max_right_minus=34	max_right_plus=33	neg_log10_pos_hash_p_value=0.0	pos_hash_score=54	side_1_annotate_key=gene	side_1_overlap=0	side_1_redundant=0	side_2_annotate_key=gene	side_2_overlap=0	side_2_redundant=0	total_non_overlap_reads=146
 JC	3		NC_001416-1	9700	-1	NC_001416-2	1	1	0	alignment_overlap=0	coverage_minus=36	coverage_plus=57	flanking_left=35	flanking_right=35	key=NC_001416-1__9700__-1__NC_001416-2__1__1__0____35__35__0__0	max_left=34	max_left_minus=33	max_left_plus=34	max_min_left=17	max_min_left_minus=17	max_min_left_plus=16	max_min_right=17	max_min_right_minus=17	max_min_right_plus=17	max_pos_hash_score=68	max_right=34	max_right_minus=34	max_right_plus=34	neg_log10_pos_hash_p_value=0.0	pos_hash_score=48	side_1_annotate_key=gene	side_1_overlap=0	side_1_redundant=0	side_2_annotate_key=gene	side_2_overlap=0	side_2_redundant=0	total_non_overlap_reads=93
 JC	5		NC_001416-2	2337	-1	NC_001416-2	8334	1	0	alignment_overlap=5	coverage_minus=35	coverage_plus=41	flanking_left=35	flanking_right=35	key=NC_001416-2__2337__-1__NC_001416-2__8329__1__5____35__35__0__0	max_left=29	max_left_minus=27	max_left_plus=29	max_min_left=13	max_min_left_minus=13	max_min_left_plus=11	max_min_right=15	max_min_right_minus=15	max_min_right_plus=15	max_pos_hash_score=58	max_right=29	max_right_minus=29	max_right_plus=28	neg_log10_pos_hash_p_value=0.0	pos_hash_score=40	side_1_annotate_key=gene	side_1_overlap=5	side_1_redundant=0	side_2_annotate_key=gene	side_2_overlap=0	side_2_redundant=0	total_non_overlap_reads=76
 JC	4		NC_001416-2	9701	-1	NC_001416-3	1	1	0	alignment_overlap=0	coverage_minus=42	coverage_plus=28	flanking_left=35	flanking_right=35	key=NC_001416-2__9701__-1__NC_001416-3__1__1__0____35__35__0__0	max_left=34	max_left_minus=33	max_left_plus=34	max_min_left=16	max_min_left_minus=16	max_min_left_plus=15	max_min_right=17	max_min_right_minus=17	max_min_right_plus=12	max_pos_hash_score=68	max_right=34	max_right_minus=34	max_right_plus=32	neg_log10_pos_hash_p_value=0.0	pos_hash_score=42	side_1_annotate_key=gene	side_1_overlap=0	side_1_redundant=0	side_2_annotate_key=gene	side_2_overlap=0	side_2_redundant=0	total_non_overlap_reads=70
 JC	1		NC_001416-3	9700	-1	NC_001416-4	1	1	0	alignment_overlap=0	coverage_minus=71	coverage_plus=83	flanking_left=35	flanking_right=35	key=NC_001416-3__9700__-1__NC_001416-4__1__1__0____35__35__0__0	max_left=34	max_left_minus=34	max_left_plus=34	max_min_left=16	max_min_left_minus=16	max_min_left_plus=16	max_min_right=17	max_min_right_minus=17	max_min_right_plus=17	max_pos_hash_score=68	max_right=34	max_right_minus=34	max_right_plus=33	neg_log10_pos_hash_p_value=0.1	pos_hash_score=56	side_1_annotate_key=gene	side_1_overlap=0	side_1_redundant=0	side_2_annotate_key=gene	side_2_overlap=0	side_2_redundant=0	total_non_overlap_reads=154
-UN	60		NC_001416-0	1	8
-UN	61		NC_001416-2	2338	8333
-UN	62		NC_001416-4	9693	9701
+UN	48		NC_001416-0	1	8
+UN	49		NC_001416-2	2338	8333
+UN	50		NC_001416-4	9693	9701
