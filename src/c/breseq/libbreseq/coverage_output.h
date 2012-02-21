6156bbf8176d00772645b814937d1b1721a1a498
Matthew Strand <Matthew Strand@localhost>
2012-02-21 16:20:23 -0600

    RA evidence the predicts repeating INSertions will call them AMPlifications if size 2 or greater.  Normalized GDs will now alter type from INS to AMP in scenarios where the INS repeats previous sequence.

---
+++ src/c/breseq/libbreseq/coverage_output.h
@@ -0,0 +1,104 @@
+/*****************************************************************************
+ * 
+ * AUTHORS
+ * 
+ *  Jeffrey E. Barrick <jeffrey.e.barrick@gmail.com>
+ *  David B. Knoester
+ * 
+ * LICENSE AND COPYRIGHT
+ * 
+ *  Copyright (c) 2008-2010 Michigan State University
+ *  Copyright (c) 2011 The University of Texas at Austin
+ * 
+ *  breseq is free software; you can redistribute it and/or modify it under the
+ *  terms the GNU General Public License as published by the Free Software
+ *  Foundation; either version 1, or (at your option) any later version.
+ * 
+ *****************************************************************************/
+
+#ifndef _BRESEQ_COVERAGE_OUTPUT_H_
+#define _BRESEQ_COVERAGE_OUTPUT_H_
+
+#include "common.h"
+#include "pileup_base.h"
+#include "pileup.h"
+
+using namespace std;
+
+namespace breseq
+{
+  
+  /*! This class is a FACTORY for generating coverage plots
+   */
+  class coverage_output : public pileup_base
+  {
+  protected:
+    string    m_output_format; 
+    uint32_t  m_downsample;
+    bool      m_total_only;
+    uint32_t  m_shaded_flanking;
+    
+    string    m_r_script_file_name;
+    string    m_intermediate_path;
+    string    m_read_begin_output_file_name; // extra output file set as option
+    string    m_gc_output_file_name;         // extra output file set as option
+    
+    // variables used during tabulate pileup to collect and print statistics
+    ofstream  m_output_table;
+    ofstream  m_read_begin_output; 
+    ofstream  m_gc_output;
+    
+    std::map<std::string,uint32_t> m_read_begin_top_bins;
+    std::map<std::string,uint32_t> m_read_begin_bot_bins;
+    std::map<std::string,uint32_t> m_ref_begin_top_bins;
+    std::map<std::string,uint32_t> m_ref_begin_bot_bins;
+    
+    std::vector<uint32_t> m_gc_content_bins;
+    
+    //! Clear saved statistics before beginning a new table
+    void clear();
+    
+    //! Called for each alignment.
+		virtual void pileup_callback(const pileup& p);
+    
+		//! Called at end of fragment.
+    void at_target_end(const uint32_t tid);
+    
+  public:
+    
+    coverage_output( const string& bam, const string& fasta, const string& r_script_file_name, const string& intermediate_path = "/tmp" )
+      : pileup_base(bam, fasta), m_output_format("png"), m_downsample(0), m_total_only(false)
+      , m_shaded_flanking(0), m_r_script_file_name(r_script_file_name), m_intermediate_path(intermediate_path) {};
+    
+    // Get/Set Options
+
+    string output_format(const string& _output_format = "") 
+    { 
+      if (_output_format.length() > 0)
+      {
+        m_output_format = to_lower(_output_format);
+        ASSERT( (m_output_format=="png") || (m_output_format=="pdf"), 
+                "Unrecognized coverage plot output format '" + m_output_format + "'.\nValid options are: png or pdf");
+      }
+      return m_output_format; 
+    }
+    
+    bool shaded_flanking(uint32_t _shaded_flanking) 
+      { m_shaded_flanking = _shaded_flanking; return m_shaded_flanking; }
+    
+    string read_begin_output_file_name(const string& _fn) 
+      { m_read_begin_output_file_name = _fn; return m_read_begin_output_file_name; }
+    
+    string gc_output_file_name(const string& _fn) 
+      { m_gc_output_file_name = _fn; return m_gc_output_file_name; }
+    
+    bool total_only(bool _total_only)
+      { m_total_only = _total_only; return m_total_only; }
+    
+    void plot(const string& region, const string& output_file_name, uint32_t resolution = 600);
+    void table(const string& region, const string& output_file_name, uint32_t resolution = 0);
+  };
+  
+  
+}//end namespace breseq
+#endif
---
+++ src/c/breseq/rna_seq.cpp
@@ -0,0 +1,177 @@
+
+/*****************************************************************************
+
+AUTHORS
+
+  Jeffrey E. Barrick <jeffrey.e.barrick@gmail.com>
+  David B. Knoester
+
+LICENSE AND COPYRIGHT
+
+  Copyright (c) 2008-2010 Michigan State University
+  Copyright (c) 2011 The University of Texas at Austin
+
+  breseq is free software; you can redistribute it and/or modify it under the
+  terms the GNU General Public License as published by the Free Software
+  Foundation; either version 1, or (at your option) any later version.
+
+*****************************************************************************/
+
+#include "libbreseq/rna_seq.h"
+
+using namespace std;
+
+namespace breseq {
+
+string good_gene_name(const string& gene, const string& product) {
+  string ggn;
+  ggn += gene;
+  ggn += "|";
+  ggn += product;
+  ggn = substitute(ggn, " ", "_");
+  return ggn;
+}
+  
+void RNASeq::tam_to_gene_counts(cReferenceSequences& ref_seq_info, const string& fasta_file_name, const vector<string>& tam_files, const string& out_tab_file_name, bool verbose)
+{
+  int32_t num_datasets = tam_files.size();
+  
+  ofstream output_tab_file(out_tab_file_name.c_str());
+  ASSERT(output_tab_file.good(), "Error opening output file: " + out_tab_file_name);
+
+  //initialize list to zeros
+  map<string,vector<int32_t> > gene_counts;
+  
+  for(vector<cAnnotatedSequence>::iterator it_as = ref_seq_info.begin(); it_as < ref_seq_info.end(); it_as++) {
+    for (cSequenceFeatureList::iterator itr_feat=it_as->m_genes.begin(); itr_feat!=it_as->m_genes.end(); ++itr_feat) {
+      Gene g(**itr_feat);
+      string ggn = good_gene_name(g.name, g.product);
+      gene_counts[ggn] = vector<int32_t>(num_datasets, 0);
+    }
+  }
+  
+  size_t i = 0;
+  for(vector<string>::const_iterator tfi=tam_files.begin(); tfi != tam_files.end(); ++tfi) {
+    cout << "SAM File: " << *tfi << endl;
+    
+    uint32_t num_unmapped = 0;
+    uint32_t num_not_in_genes = 0;
+    uint32_t num_in_genes = 0;
+    uint32_t num_multiple_mapped = 0;
+    
+    tam_file tf(*tfi, fasta_file_name, ios_base::in);
+
+    alignment_list al;
+    
+    cSequenceFeaturePtr best_gene(NULL);
+    size_t pre_file_total = 0;
+
+    while(tf.read_alignments(al)) {
+      
+      bam_alignment* a = al.front().get();
+
+      
+      pre_file_total++;
+      if (pre_file_total % 100000 == 0) 
+        cout << "  Aligned reads processed: " << pre_file_total << endl;
+      
+      if (verbose) {
+        cout << " >" << a->read_name() << endl;
+        cout << "  " << a->reference_target_id() << ":" << a->reference_start_1() << "-" << a->reference_end_1() 
+          << "(" << a->strand() << ")" << endl;
+      }
+      
+      
+      // Ignore multiple alignments
+      if (al.size() != 1) {
+        if (verbose)
+          cout << "  Multiple alignments (not counted)"<< endl;
+        
+        num_multiple_mapped++;
+        continue;
+      }
+      
+      if (a->unmapped()) {
+        
+        if (verbose)
+          cout << "  Unmapped (not counted)"<< endl;
+
+        num_unmapped++; 
+        continue;
+      }
+      
+      uint32_t tid = a->reference_target_id();
+      
+      cAnnotatedSequence& this_ref_seq = ref_seq_info[a->reference_target_id()];
+      
+      int32_t best_overlap = 0;
+      for (cSequenceFeatureList::iterator itr_feat=this_ref_seq.m_genes.begin(); itr_feat!=this_ref_seq.m_genes.end(); ++itr_feat) {
+
+        cSequenceFeature& feat = **itr_feat;
+        
+        if (feat.m_strand != a->strand())
+          continue;
+        
+        int32_t ulength = overlap_length(feat.m_start, feat.m_end, a->reference_start_1(), a->reference_end_1());
+        
+        if (ulength > best_overlap) {
+          best_overlap = ulength;
+          best_gene = *itr_feat;
+        }
+      }
+      
+      if (best_gene.get()) {
+        Gene g(*best_gene);
+        
+        if (verbose)
+          cout << "  Best gene: " << g.name << " " << g.product << " " << best_gene->m_start << "-" << best_gene->m_end 
+            << "(" << static_cast<int32_t>(best_gene->m_strand) << ")" << endl;
+        
+
+        // because we print tab-delimited, they can really screw up our columns
+        string safe_gene_name = substitute(g.name, "\t", " ");
+        string ggn = good_gene_name(g.name, g.product);
+        gene_counts[ggn][i]++;
+        
+        //cerr << g.name << endl;
+        num_in_genes++;
+      }
+      else {
+        if (verbose)
+          cout << "  Not in gene (not counted)"<< endl;
+        
+        num_not_in_genes++;
+      }
+    }
+    
+    cout << "  Unmapped: " << num_unmapped << endl;
+    cout << "  Multiple mapped: " << num_multiple_mapped << endl;
+    cout << "  Not in genes: " << num_not_in_genes << endl;
+    cout << "  In genes: " << num_in_genes << endl;
+    
+    i++;
+  }
+  
+  // Print out
+  
+  // One column for each data set
+  
+  output_tab_file << "gene";
+  for(vector<string>::const_iterator it=tam_files.begin(); it!=tam_files.end(); it++) {
+    output_tab_file << "\t" << *it;
+  }
+  output_tab_file << endl;
+  
+  // Each data row
+  for(map<string,vector<int32_t> >::iterator mit=gene_counts.begin(); mit!=gene_counts.end(); mit++) {
+    
+    output_tab_file << mit->first;
+    
+    for(vector<int32_t>::iterator cit=mit->second.begin(); cit!=mit->second.end(); cit++) {
+      output_tab_file << "\t" << *cit;
+    }
+    output_tab_file << endl;
+  }
+}
+
+} // namespace breseq
