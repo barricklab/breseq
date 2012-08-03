bff5dfb81357dcf0034137cc749e5a58342161da
Geoffrey Colburn <Geoffrey Colburn@localhost>
2012-08-03 17:45:44 -0500

    Now tests have been updated to account for locus_tags.

--- tests/REL606_fragment_deletion_between_is/expected.gd
+++ tests/REL606_fragment_deletion_between_is/expected.gd
@@ -2,8 +2,8 @@
 #=AUTHOR	
 #=REFSEQ	tests/REL606_fragment_deletion_between_is/../data/REL606/REL606.fragment.gbk
 #=READSEQ	tests/REL606_fragment_deletion_between_is/../data/REL606/REL606.fragment.2.fastq
-DEL	8	6	REL606-5	10535	21535	between=IS1
-MC	6		REL606-5	10229	32044	305	298	left_inside_cov=15	left_outside_cov=18	right_inside_cov=15	right_outside_cov=17
+DEL	8	6	REL606-5	10535	21535	between=IS1	gene_name=[ECB_00212]–[phoE]	gene_product=<i><b>27 genes</b><BR>[ECB_00212], yhhI, yafV, ykfE, fadE, lpcA, yafJ, yafK, yafQ, dinJ, yafL, yafM, fhiA, mbhA, dinB, yafN, yafO, yafP, ykfJ, prfH, pepD, gpt, yafA, crl, insA-3, insB-3, [phoE]</i>	locus_tag=[ECB_00212]–[ECB_00238]
+MC	6		REL606-5	10229	32044	305	298	gene_name=[insB-2]–[insB-3]	gene_product=<i><b>27 genes</b><BR>[insB-2], ECB_00212, yhhI, yafV, ykfE, fadE, lpcA, yafJ, yafK, yafQ, dinJ, yafL, yafM, fhiA, mbhA, dinB, yafN, yafO, yafP, ykfJ, prfH, pepD, gpt, yafA, crl, insA-3, [insB-3]</i>	left_inside_cov=15	left_outside_cov=18	locus_tag=[ECB_00211]–[ECB_00237]	right_inside_cov=15	right_outside_cov=17
 JC	1		REL606-5	1	1	REL606-5	46298	-1	0	alignment_overlap=0	circular_chromosome=1	continuation_left=0	continuation_right=0	coverage_minus=14	coverage_plus=13	flanking_left=36	flanking_right=36	key=REL606-5__1__1__REL606-5__46298__-1__0____36__36__0__0	max_left=34	max_left_minus=33	max_left_plus=34	max_min_left=16	max_min_left_minus=15	max_min_left_plus=16	max_min_right=18	max_min_right_minus=18	max_min_right_plus=17	max_pos_hash_score=70	max_right=30	max_right_minus=29	max_right_plus=30	neg_log10_pos_hash_p_value=0.0	new_junction_coverage=0.772596	new_junction_read_count=27	pos_hash_score=21	side_1_annotate_key=gene	side_1_coverage=0	side_1_overlap=0	side_1_read_count=0	side_1_redundant=0	side_2_annotate_key=gene	side_2_coverage=0	side_2_overlap=0	side_2_read_count=0	side_2_redundant=0	total_non_overlap_reads=27
 UN	2		REL606-5	389	408
 UN	3		REL606-5	8392	8411
