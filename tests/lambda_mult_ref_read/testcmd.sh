ad6f1ac6ed2e18926cfebee95eaf3313721f4eb0
Jeffrey Barrick <Jeffrey Barrick@localhost>
2013-04-28 21:13:16 -0500

    Fixed bug crashing on lowercase base in non-last reference sequence within the same file. Upddated test to detect this error.

--- tests/lambda_mult_ref_read/testcmd.sh
+++ tests/lambda_mult_ref_read/testcmd.sh
@@ -6,8 +6,7 @@ SELF=`dirname ${BASH_SOURCE}`
 TESTCMD="\
     ${BRESEQ} \
         -o ${SELF} \
-        -r ${DATADIR}/lambda/lambda.1.gbk \
-        -r ${DATADIR}/lambda/lambda.2.gbk \
+        -r ${DATADIR}/lambda/lambda.1-2.gbk \
         -r ${DATADIR}/lambda/lambda.3.gbk \
         -r ${DATADIR}/lambda/lambda.4.gbk \
         -r ${DATADIR}/lambda/lambda.5.gbk \
