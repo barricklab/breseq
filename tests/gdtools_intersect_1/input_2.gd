4f920d1d556dfb6865db91caa053af339a91d068
Jeffrey Barrick <Jeffrey Barrick@localhost>
2014-12-14 13:35:52 -0600

    Added gdtools tests

---
+++ tests/gdtools_intersect_1/input_2.gd
@@ -0,0 +1,126 @@
+#=GENOME_DIFF	1.0
+SNP	1	23	ref	830180	G
+INS	2	24	ref	866932	G
+SNP	3	25	ref	1440026	C
+SNP	4	26	ref	1564307	T
+SNP	5	27	ref	1634375	T
+SNP	6	28	ref	1717943	T
+SNP	7	29	ref	1911017	T
+DEL	8	31	ref	2304200	1
+SNP	9	32	ref	2654787	G
+SNP	10	33	ref	2998152	T
+DEL	11	48,54	ref	3589286	663
+SNP	12	34	ref	4467282	T
+SNP	13	36	ref	5391338	A
+SNP	14	37	ref	5606557	C
+SNP	15	38	ref	5893144	C
+SNP	16	39	ref	5927894	C
+SNP	17	43	ref	6287052	A
+SNP	18	44	ref	6287570	A
+SNP	19	45	ref	6971131	C
+SNP	20	46	ref	7101832	C
+DEL	21	49,56	ref	7158853	561
+SNP	22	47	ref	8716414	T
+RA	23		ref	830180	0	T	G	genotype_quality=648.9	new_cov=49/108	quality=648.9	ref_cov=0/0	tot_cov=49/108
+RA	24		ref	866932	1	.	G	genotype_quality=630.1	new_cov=52/100	quality=630.1	ref_cov=0/0	tot_cov=52/100
+RA	25		ref	1440026	0	T	C	genotype_quality=130.0	new_cov=17/15	quality=130.0	ref_cov=0/0	tot_cov=17/15
+RA	26		ref	1564307	0	C	T	genotype_quality=440.8	new_cov=64/41	quality=440.8	ref_cov=0/0	tot_cov=64/41
+RA	27		ref	1634375	0	G	T	genotype_quality=503.7	new_cov=53/68	quality=503.7	ref_cov=0/0	tot_cov=53/68
+RA	28		ref	1717943	0	G	T	genotype_quality=631.4	new_cov=60/90	quality=631.4	ref_cov=0/0	tot_cov=60/90
+RA	29		ref	1911017	0	C	T	genotype_quality=673.1	new_cov=68/91	quality=673.1	ref_cov=0/0	tot_cov=68/91
+RA	30		ref	2035983	0	G	T	bias_e_value=6340870	bias_p_value=0.693768	fisher_strand_p_value=0.328119	frequency=0.1307	genotype_quality=571.9	ks_quality_p_value=1	log10_base_likelihood=-70.2747	log10_qual_likelihood_position_model=2.30514	log10_strand_likelihood_position_model=0.979274	mixed=1	new_cov=9/14	polymorphism_quality=65.0	quality=65.0	quality_position_model=2.01102e-69	ref_cov=43/110	tot_cov=52/124
+RA	31		ref	2304200	0	T	.	genotype_quality=614.8	new_cov=53/68	quality=614.8	ref_cov=0/0	tot_cov=53/68
+RA	32		ref	2654787	0	A	G	genotype_quality=717.6	new_cov=94/79	quality=717.6	ref_cov=0/0	tot_cov=94/79
+RA	33		ref	2998152	0	C	T	genotype_quality=720.8	new_cov=113/60	quality=720.8	ref_cov=0/0	tot_cov=113/60
+RA	34		ref	4467282	0	C	T	genotype_quality=455.0	new_cov=57/51	quality=455.0	ref_cov=0/0	tot_cov=57/51
+RA	35		ref	5377066	0	C	T	bias_e_value=6037120	bias_p_value=0.660534	fisher_strand_p_value=0.313977	frequency=0.8630	genotype_quality=216.4	ks_quality_p_value=0.953746	log10_base_likelihood=-28.6567	log10_qual_likelihood_position_model=2.2307	log10_strand_likelihood_position_model=0.789417	mixed=1	new_cov=42/21	polymorphism_quality=23.2	quality=23.2	quality_position_model=7.3028e-28	ref_cov=5/5	tot_cov=47/26
+RA	36		ref	5391338	0	G	A	genotype_quality=536.2	new_cov=53/75	quality=536.2	ref_cov=0/0	tot_cov=53/75
+RA	37		ref	5606557	0	G	C	genotype_quality=818.8	new_cov=118/81	quality=818.8	ref_cov=0/0	tot_cov=119/81
+RA	38		ref	5893144	0	A	C	genotype_quality=39.0	new_cov=0/11	quality=39.0	ref_cov=0/0	tot_cov=0/11
+RA	39		ref	5927894	0	A	C	genotype_quality=490.1	new_cov=49/70	quality=490.1	ref_cov=0/0	tot_cov=49/70
+RA	40		ref	5998849	0	C	T	bias_e_value=4546880	bias_p_value=0.497483	fisher_strand_p_value=0.187713	frequency=0.5411	genotype_quality=89.0	ks_quality_p_value=0.986542	log10_base_likelihood=-364.815	log10_qual_likelihood_position_model=2.71382	log10_strand_likelihood_position_model=1.38987	mixed=1	new_cov=68/57	polymorphism_quality=inf	quality=inf	quality_position_model=0	ref_cov=48/58	tot_cov=116/115
+RA	41		ref	5998879	0	T	C	bias_e_value=8609590	bias_p_value=0.941993	fisher_strand_p_value=0.790514	frequency=0.5244	genotype_quality=29.0	ks_quality_p_value=0.859356	log10_base_likelihood=-377.869	log10_qual_likelihood_position_model=5.54742	log10_strand_likelihood_position_model=0.988581	mixed=1	new_cov=53/65	polymorphism_quality=inf	quality=inf	quality_position_model=0	ref_cov=50/57	tot_cov=103/122
+RA	42		ref	5999065	0	T	C	bias_e_value=7014150	bias_p_value=0.767433	fisher_strand_p_value=0.702261	frequency=0.4919	genotype_quality=27.8	ks_quality_p_value=0.571015	log10_base_likelihood=-410.868	log10_qual_likelihood_position_model=6.83563	log10_strand_likelihood_position_model=1.02778	mixed=1	new_cov=68/53	polymorphism_quality=inf	quality=inf	quality_position_model=0	ref_cov=67/58	tot_cov=135/111
+RA	43		ref	6287052	0	G	A	genotype_quality=332.6	new_cov=37/44	quality=332.6	ref_cov=0/0	tot_cov=37/44
+RA	44		ref	6287570	0	C	A	genotype_quality=374.4	new_cov=48/41	quality=374.4	ref_cov=0/0	tot_cov=48/41
+RA	45		ref	6971131	0	G	C	genotype_quality=575.8	new_cov=66/73	quality=575.8	ref_cov=0/0	tot_cov=66/73
+RA	46		ref	7101832	0	T	C	genotype_quality=494.4	new_cov=78/41	quality=494.4	ref_cov=0/0	tot_cov=78/41
+RA	47		ref	8716414	0	G	T	genotype_quality=821.2	new_cov=89/108	quality=821.2	ref_cov=0/0	tot_cov=89/108
+MC	48		ref	3589286	3589948	0	0	left_inside_cov=0	left_outside_cov=88	right_inside_cov=0	right_outside_cov=86
+MC	49		ref	7158853	7159413	0	0	left_inside_cov=0	left_outside_cov=68	right_inside_cov=0	right_outside_cov=68
+JC	50		ref	1	1	ref	9139763	-1	0	alignment_overlap=0	circular_chromosome=1	coverage_minus=80	coverage_plus=34	flanking_left=125	flanking_right=125.0000	junction_possible_overlap_registers=121	key=ref__1__1__ref__9139763__-1__0____125__125__0__0	max_left=124	max_left_minus=124	max_left_plus=109	max_min_left=62	max_min_left_minus=61	max_min_left_plus=62	max_min_right=60	max_min_right_minus=58	max_min_right_plus=60	max_pos_hash_score=242	max_right=121	max_right_minus=120	max_right_plus=121	neg_log10_pos_hash_p_value=0.6	new_junction_coverage=0.75	new_junction_frequency=1.0000	new_junction_read_count=115	pos_hash_score=88	side_1_annotate_key=gene	side_1_continuation=0	side_1_coverage=0.00	side_1_overlap=0	side_1_possible_overlap_registers=121	side_1_read_count=0	side_1_redundant=0	side_2_annotate_key=gene	side_2_continuation=0	side_2_coverage=0.00	side_2_overlap=0	side_2_possible_overlap_registers=121	side_2_read_count=0	side_2_redundant=0	total_non_overlap_reads=114
+JC	51		ref	648832	-1	ref	648836	1	0	alignment_overlap=0	coverage_minus=3	coverage_plus=0	flanking_left=125	flanking_right=125	frequency=0.1608	junction_possible_overlap_registers=121	key=ref__648832__-1__ref__648836__1__0____125__125__0__0	max_left=71	max_left_minus=71	max_left_plus=0	max_min_left=29	max_min_left_minus=29	max_min_left_plus=0	max_min_right=54	max_min_right_minus=54	max_min_right_plus=0	max_pos_hash_score=210	max_right=109	max_right_minus=109	max_right_plus=0	neg_log10_pos_hash_p_value=9.1	new_junction_coverage=0.24	new_junction_frequency=0.1608	new_junction_read_count=36	pos_hash_score=3	reject=NJ	side_1_annotate_key=gene	side_1_continuation=16	side_1_coverage=1.23	side_1_overlap=0	side_1_possible_overlap_registers=105	side_1_read_count=163	side_1_redundant=0	side_2_annotate_key=gene	side_2_continuation=0	side_2_coverage=1.23	side_2_overlap=0	side_2_possible_overlap_registers=121	side_2_read_count=188	side_2_redundant=0	total_non_overlap_reads=3
+JC	52		ref	680927	-1	ref	681030	1	0	alignment_overlap=8	coverage_minus=1	coverage_plus=2	flanking_left=125	flanking_right=125	frequency=0.0121	junction_possible_overlap_registers=113	key=ref__680927__-1__ref__681022__1__8____125__125__0__0	max_left=81	max_left_minus=28	max_left_plus=81	max_min_left=34	max_min_left_minus=28	max_min_left_plus=34	max_min_right=36	max_min_right_minus=0	max_min_right_plus=36	max_pos_hash_score=226	max_right=89	max_right_minus=89	max_right_plus=83	neg_log10_pos_hash_p_value=9.4	new_junction_coverage=0.02	new_junction_frequency=0.0121	new_junction_read_count=3	pos_hash_score=3	reject=NJ	side_1_annotate_key=gene	side_1_continuation=0	side_1_coverage=1.61	side_1_overlap=8	side_1_possible_overlap_registers=121	side_1_read_count=247	side_1_redundant=0	side_2_annotate_key=gene	side_2_continuation=0	side_2_coverage=1.83	side_2_overlap=0	side_2_possible_overlap_registers=113	side_2_read_count=261	side_2_redundant=0	total_non_overlap_reads=3
+JC	53		ref	2730374	-1	ref	2730693	1	0	alignment_overlap=8	coverage_minus=2	coverage_plus=5	flanking_left=125	flanking_right=125	frequency=0.0358	junction_possible_overlap_registers=113	key=ref__2730374__-1__ref__2730685__1__8____125__125__0__0	max_left=111	max_left_minus=111	max_left_plus=66	max_min_left=57	max_min_left_minus=48	max_min_left_plus=57	max_min_right=57	max_min_right_minus=6	max_min_right_plus=57	max_pos_hash_score=226	max_right=83	max_right_minus=66	max_right_plus=83	neg_log10_pos_hash_p_value=7.7	new_junction_coverage=0.05	new_junction_frequency=0.0358	new_junction_read_count=7	pos_hash_score=7	reject=NJ	side_1_annotate_key=gene	side_1_continuation=0	side_1_coverage=1.35	side_1_overlap=8	side_1_possible_overlap_registers=121	side_1_read_count=207	side_1_redundant=0	side_2_annotate_key=gene	side_2_continuation=0	side_2_coverage=1.29	side_2_overlap=0	side_2_possible_overlap_registers=113	side_2_read_count=184	side_2_redundant=0	total_non_overlap_reads=7
+JC	54		ref	3589285	-1	ref	3589949	1	0	alignment_overlap=0	coverage_minus=46	coverage_plus=40	flanking_left=125	flanking_right=125.0000	junction_possible_overlap_registers=121	key=ref__3589285__-1__ref__3589949__1__0____125__125__0__0	max_left=118	max_left_minus=116	max_left_plus=118	max_min_left=62	max_min_left_minus=61	max_min_left_plus=62	max_min_right=61	max_min_right_minus=61	max_min_right_plus=59	max_pos_hash_score=242	max_right=124	max_right_minus=124	max_right_plus=120	neg_log10_pos_hash_p_value=1.3	new_junction_coverage=0.56	new_junction_frequency=1.0000	new_junction_read_count=86	pos_hash_score=67	side_1_annotate_key=gene	side_1_continuation=0	side_1_coverage=0.00	side_1_overlap=0	side_1_possible_overlap_registers=121	side_1_read_count=0	side_1_redundant=0	side_2_annotate_key=gene	side_2_continuation=0	side_2_coverage=0.00	side_2_overlap=0	side_2_possible_overlap_registers=121	side_2_read_count=0	side_2_redundant=0	total_non_overlap_reads=86
+JC	55		ref	5998808	1	ref	5999362	-1	0	alignment_overlap=29	coverage_minus=58	coverage_plus=42	flanking_left=125	flanking_right=125	frequency=0.5229	junction_possible_overlap_registers=92	key=ref__5998808__1__ref__5999391__-1__29____125__125__0__0	max_left=94	max_left_minus=94	max_left_plus=93	max_min_left=46	max_min_left_minus=46	max_min_left_plus=42	max_min_right=47	max_min_right_minus=42	max_min_right_plus=47	max_pos_hash_score=184	max_right=94	max_right_minus=93	max_right_plus=94	neg_log10_pos_hash_p_value=0.4	new_junction_coverage=0.88	new_junction_frequency=0.5229	new_junction_read_count=102	pos_hash_score=74	side_1_annotate_key=gene	side_1_continuation=0	side_1_coverage=0.71	side_1_overlap=29	side_1_possible_overlap_registers=121	side_1_read_count=108	side_1_redundant=0	side_2_annotate_key=gene	side_2_continuation=0	side_2_coverage=0.89	side_2_overlap=0	side_2_possible_overlap_registers=92	side_2_read_count=104	side_2_redundant=0	total_non_overlap_reads=100
+JC	56		ref	7158852	-1	ref	7159414	1	0	alignment_overlap=2	coverage_minus=37	coverage_plus=31	flanking_left=125	flanking_right=125.0000	junction_possible_overlap_registers=119	key=ref__7158852__-1__ref__7159412__1__2____125__125__0__0	max_left=122	max_left_minus=122	max_left_plus=122	max_min_left=61	max_min_left_minus=59	max_min_left_plus=61	max_min_right=61	max_min_right_minus=51	max_min_right_plus=61	max_pos_hash_score=238	max_right=120	max_right_minus=120	max_right_plus=116	neg_log10_pos_hash_p_value=1.4	new_junction_coverage=0.45	new_junction_frequency=1.0000	new_junction_read_count=68	pos_hash_score=63	side_1_annotate_key=gene	side_1_continuation=0	side_1_coverage=0.00	side_1_overlap=2	side_1_possible_overlap_registers=121	side_1_read_count=0	side_1_redundant=0	side_2_annotate_key=gene	side_2_continuation=0	side_2_coverage=0.00	side_2_overlap=0	side_2_possible_overlap_registers=119	side_2_read_count=0	side_2_redundant=0	total_non_overlap_reads=68
+JC	57		ref	7520016	-1	ref	7520347	1	0	alignment_overlap=6	coverage_minus=3	coverage_plus=3	flanking_left=125	flanking_right=125	frequency=0.0279	junction_possible_overlap_registers=115	key=ref__7520016__-1__ref__7520341__1__6____125__125__0__0	max_left=111	max_left_minus=90	max_left_plus=111	max_min_left=5	max_min_left_minus=5	max_min_left_plus=0	max_min_right=40	max_min_right_minus=40	max_min_right_plus=40	max_pos_hash_score=230	max_right=114	max_right_minus=114	max_right_plus=40	neg_log10_pos_hash_p_value=8.2	new_junction_coverage=0.04	new_junction_frequency=0.0279	new_junction_read_count=6	pos_hash_score=6	reject=NJ	side_1_annotate_key=gene	side_1_continuation=0	side_1_coverage=1.39	side_1_overlap=6	side_1_possible_overlap_registers=121	side_1_read_count=213	side_1_redundant=0	side_2_annotate_key=gene	side_2_continuation=0	side_2_coverage=1.48	side_2_overlap=0	side_2_possible_overlap_registers=115	side_2_read_count=216	side_2_redundant=0	total_non_overlap_reads=6
+UN	58		ref	149892	149892
+UN	59		ref	376331	378899
+UN	60		ref	379111	381944
+UN	61		ref	661540	662183
+UN	62		ref	935709	935727
+UN	63		ref	1546286	1546839
+UN	64		ref	1547077	1547293
+UN	65		ref	2035056	2035098
+UN	66		ref	2124623	2124715
+UN	67		ref	2125240	2125331
+UN	68		ref	2126000	2127184
+UN	69		ref	2376437	2376643
+UN	70		ref	2377280	2377486
+UN	71		ref	2433875	2435500
+UN	72		ref	2457601	2457701
+UN	73		ref	2458225	2458313
+UN	74		ref	2505846	2508426
+UN	75		ref	2860259	2860269
+UN	76		ref	2862651	2862657
+UN	77		ref	3292825	3294532
+UN	78		ref	3295100	3298087
+UN	79		ref	3298092	3298092
+UN	80		ref	3298095	3298095
+UN	81		ref	3589286	3589948
+UN	82		ref	3593639	3593914
+UN	83		ref	3594158	3594475
+UN	84		ref	3796624	3796795
+UN	85		ref	3796969	3797142
+UN	86		ref	3853367	3853640
+UN	87		ref	3853884	3854197
+UN	88		ref	4195995	4197247
+UN	89		ref	4209345	4211123
+UN	90		ref	4212544	4215130
+UN	91		ref	4218076	4219330
+UN	92		ref	4224998	4225055
+UN	93		ref	4231387	4231448
+UN	94		ref	4317438	4317455
+UN	95		ref	4323801	4323819
+UN	96		ref	4456715	4456745
+UN	97		ref	4876814	4877995
+UN	98		ref	4883155	4884344
+UN	99		ref	5537676	5538272
+UN	100		ref	5893169	5894521
+UN	101		ref	5894757	5896400
+UN	102		ref	5897048	5898762
+UN	103		ref	5925650	5925670
+UN	104		ref	6070038	6070648
+UN	105		ref	6070839	6070872
+UN	106		ref	6151613	6152263
+UN	107		ref	6500925	6502105
+UN	108		ref	6509241	6510945
+UN	109		ref	6749087	6749109
+UN	110		ref	7158853	7159413
+UN	111		ref	7184372	7184636
+UN	112		ref	7185005	7185269
+UN	113		ref	7185731	7185753
+UN	114		ref	7313443	7313671
+UN	115		ref	7313905	7314454
+UN	116		ref	7572900	7574082
+UN	117		ref	7742364	7743014
+UN	118		ref	7763640	7766217
+UN	119		ref	7769113	7770368
+UN	120		ref	7997189	7997207
+UN	121		ref	8961539	8962386
+UN	122		ref	8962600	8962873
+UN	123		ref	8963099	8964055
+UN	124		ref	8964232	8966828
+UN	125		ref	9049883	9050529
