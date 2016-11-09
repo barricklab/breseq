a5466e2338aaf8e37702b4ba4911311a0dffb8a0
jeffreybarrick <jeffrey.e.barrick@gmail.com>
2016-11-09 01:07:22 -0600

    Fix for SAM file input with nonstandard bases; added test for this

---
+++ tests/lambda_polymorphism_aligned_sam/testcmd.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+SELF=`dirname ${BASH_SOURCE}`
+. ${SELF}/../common.sh
+
+CURRENT_OUTPUTS[0]="output/evidence/annotated.gd"
+EXPECTED_OUTPUTS[0]="expected.gd"
+REFERENCE_ARG="-r ${DATADIR}/lambda/lambda.gbk"
+
+TESTCMD="\
+    ${BRESEQ} \
+    -p \
+    --aligned-sam \
+    -o ${SELF} \
+    ${REFERENCE_ARG} \
+    ${DATADIR}/lambda/lambda_mixed_population.sam \
+    "
+
+do_test $1 ${SELF}
