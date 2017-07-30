61cd02980f1ff0cb6a965df8ceab768822ef4793
jeffreybarrick <jeffrey.e.barrick@gmail.com>
2017-07-30 08:24:49 -0500

    Updated copyright headers

--- src/c/breseq/coverage_distribution.r
+++ src/c/breseq/coverage_distribution.r
@@ -1,20 +1,19 @@
-/*****************************************************************************
- 
- AUTHORS
- 
- Jeffrey E. Barrick <jeffrey.e.barrick@gmail.com>
- David B. Knoester
- 
- LICENSE AND COPYRIGHT
- 
- Copyright (c) 2008-2010 Michigan State University
- Copyright (c) 2011-2017 The University of Texas at Austin
- 
- breseq is free software; you can redistribute it and/or modify it under the
- terms the GNU General Public License as published by the Free Software
- Foundation; either version 1, or (at your option) any later version.
- 
- *****************************************************************************/
+##
+##
+## AUTHORS
+##
+## Jeffrey E. Barrick <jeffrey.e.barrick@gmail.com>
+##
+## LICENSE AND COPYRIGHT
+##
+## Copyright (c) 2008-2010 Michigan State University
+## Copyright (c) 2011-2017 The University of Texas at Austin
+##
+## breseq is free software; you can redistribute it and/or modify it under the
+## terms the GNU General Public License as published by the Free Software
+## Foundation; either version 1, or (at your option) any later version.
+##
+##
 
 ## Arguments:
 ##   distribution_file=/path/to/input 
