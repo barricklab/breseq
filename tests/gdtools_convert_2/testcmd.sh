17c6566a284ace5a0ebc225aaaa14f129a200adf
jeffreybarrick <jeffrey.e.barrick@gmail.co>
2017-10-06 10:16:01 -0500

    Fixed JSON convert test to not rely on data not included in main source repo

--- tests/gdtools_convert_2/testcmd.sh
+++ tests/gdtools_convert_2/testcmd.sh
@@ -12,10 +12,7 @@ TESTCMD="\
         -a
         -f JSON
         -o ${SELF}/output.json \
-        -r ${DATADIR}/lambda/lambda.1-2.gbk \
-        -r ${DATADIR}/lambda/lambda.3.gbk \
-        -r ${DATADIR}/lambda/lambda.4.gbk \
-        -r ${DATADIR}/lambda/lambda.5.gbk \
+        -r ${DATADIR}/REL606/REL606.fragment.gbk \
         ${SELF}/input.gd \
     "
 
