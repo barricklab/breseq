b910fe0b2bf8f307958c5bca0292649c8cd5a880
Jeffrey Barrick <jeffrey.e.barrick@gmail.com>
2022-11-08 20:57:31 -0600

    Changed diefault long read split length to 200

--- src/c/breseq/libbreseq/settings.h
+++ src/c/breseq/libbreseq/settings.h
@@ -237,7 +237,7 @@ namespace breseq
     uint32_t read_file_read_length_min;         // Default = 18 COMMAND-LINE OPTION
     double read_file_max_same_base_fraction;    // Default = 0.9 COMMAND-LINE OPTION
     double read_file_max_N_fraction;            // Default = 0.5 COMMAND-LINE OPTION
-    uint32_t read_file_long_read_split_length;            // Default = 500 COMMAND-LINE OPTION
+    uint32_t read_file_long_read_split_length;            // Default = 200 COMMAND-LINE OPTION
     bool read_file_long_read_distribute_remainder;           // Default = false COMMAND-LINE OPTION
 
     // Reference sequences
